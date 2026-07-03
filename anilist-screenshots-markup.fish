ad9ef35cb69459262e27683b0a50e4d25c5b974e
Kyle Lanchman <k.lanchman@gmail.com>
2026-07-03 14:52:01 -0400

    Sort screenshots for AniList export

--- anilist-screenshots-markup.fish
+++ anilist-screenshots-markup.fish
@@ -5,7 +5,8 @@ mise x -- hugo --gc --minify
 
 set markup ""
 
-for f in public/img/screenshots/*.{png,jpeg}
+set images (find public/img/screenshots -type f -name "*.png" -o -name "*.jpeg" | sort)
+for f in $images
   set str (string replace --all " " "%20" $f)
   set str (string replace "public/" "" $str)
   set markup "$markup img220(https://ryuusei.moe/$str)"
